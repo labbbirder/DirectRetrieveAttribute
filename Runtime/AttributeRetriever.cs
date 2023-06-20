ccc11deb43249ea5273ab2eb602e719488f510f1
bbbirder <502100554@qq.com>
2023-06-20 15:40:02 +0800

    doc: add comments

--- Runtime/AttributeRetriever.cs
+++ Runtime/AttributeRetriever.cs
@@ -11,11 +11,27 @@ namespace com.bbbirder.DirectAttribute{
             | BindingFlags.NonPublic
             | BindingFlags.Static
             ;
+        /// <summary>
+        /// Retrieve Attribute T in all Assemblies
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T[] GetAll<T>() where T:DirectRetrieveAttribute{
             var attrType = typeof(T);
             return AppDomain.CurrentDomain.GetAssemblies()
                 //.Where(a=>a.GetReferencedAssemblies().Any(a=>a.ToString()== attrAssemblyName))
-                .SelectMany(a => a.GetCustomAttributes<GeneratedDirectRetrieveAttribute>())
+                .SelectMany(a => GetAll<T>(a))
+                .ToArray();
+        }
+        /// <summary>
+        /// Retrieve Attribute T on a specific Assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T[] GetAll<T>(Assembly assembly) where T:DirectRetrieveAttribute{
+            var attrType = typeof(T);
+            return assembly.GetCustomAttributes<GeneratedDirectRetrieveAttribute>()
                 .SelectMany(a => {
                     var targetType = a.type;
                     if (a.HasMemberName) {
