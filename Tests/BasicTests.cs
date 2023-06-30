69b3b2e6877517338f8d9be1f2d1185bf55a05a5
labbbirder <502100554@qq.com>
2023-06-30 10:54:16 +0800

    test: init

---
+++ Tests/BasicTests.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using com.bbbirder;
+using bt_it;
+
+public class BasicTests
+{
+    static void AssertCount<T>(int count) where T:DirectRetrieveAttribute
+    {
+        var al = Retriever.GetAllAttributes<T>(typeof(BasicTests).Assembly);
+        Assert.AreEqual(count,al.Length);
+    }
+    static void AssertSubTypeCount<T>(int count)
+    {
+        var sl = Retriever.GetAllSubtypes(typeof(BasicTests).Assembly,typeof(T));
+        Assert.AreEqual(count,sl.Length);
+    }
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestGenerics()
+    {
+        AssertCount<TGAttribute>(4);
+    }
+    [Test]
+    public void TestNest()
+    {
+        AssertCount<INNAttribute>(3);
+    }
+    [Test]
+    public void TestBaseType(){
+        AssertSubTypeCount<MyBaseType>(5);
+    }
+    [Test]
+    public void TestInterface(){
+        AssertSubTypeCount<IMyInterface>(4);
+    }
+}
+class TGAttribute:DirectRetrieveAttribute{}
+class INNAttribute:DirectRetrieveAttribute{}
+namespace innerA.innerB{
+    public class TestClassA<T>{
+        [TG]
+        public void Bar(T t){
+
+        }
+        [TG]
+        public void Foo<G>(G g){
+
+        }
+        [TG]
+        public void Foo2<G,H>(G g){
+
+        }
+    }
+    [TG]
+    internal class TestGenericB<T,F,G>{
+        [INN]
+        internal class Inner{
+            [INN]
+            static void Bar(){
+
+            }
+            [INN]
+            internal class Far{
+
+            }
+        }
+    }
+}
+namespace bt_it{
+    [InheritRetrieve]
+    class MyBaseType{
+        
+    }
+    class SubA<T>:MyBaseType{
+        internal class SubA_A<G,F>:MyBaseType{}
+    }
+    class SubB:SubA<int>{}
+    class SubC<T>:SubA<T>{}
+    class SubD:SubB{}
+
+    
+    [InheritRetrieve]
+    interface IMyInterface{
+        
+    }
+    class ImpA<T>:IMyInterface{
+        internal class ImpA_A<G,F>:IMyInterface{}
+    }
+    class ImpB:ImpA<int>{}
+    struct ImpS:IMyInterface{}
+}
