06bc070fdeb72062bfef874fbc97c7348bffeed1
labbbirder <502100554@qq.com>
2023-07-05 16:04:07 +0800

    feat: more retrieve methods

--- Runtime/Retriever.cs
+++ Runtime/Retriever.cs
@@ -29,7 +29,6 @@ namespace com.bbbirder
                 .Select(a => a.type)
                 .Distinct()
                 .ToArray();
-            foreach (var a in al) Console.WriteLine($"r: {a}");
             return AppDomain.CurrentDomain.GetAssemblies()
                 .Where(a => a.IsDefined(typeof(GeneratedDirectRetrieveAttribute)))
                 .SelectMany(a => GetAllAttributes<DirectRetrieveAttribute>(a))
@@ -45,38 +44,68 @@ namespace com.bbbirder
                 .Distinct()
                 .ToArray();
         });
+
+
         /// <summary>
-        /// Retrieve Attribute T in all Assemblies
+        /// Retrieve attributes of type T in all loaded assemblies
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static T[] GetAllAttributes<T>() where T : DirectRetrieveAttribute
         {
-            var attrType = typeof(T);
-            return attrLut.Value.ToArray()
-                .Where(a=> attrType.IsAssignableFrom(a.Key))
-                .SelectMany(a=>a.Value)
-                .OfType<T>()
-                .ToArray()
-                ;
+            return GetAllAttributes(typeof(T)).OfType<T>().ToArray();
         }
+
         /// <summary>
-        /// Retrieve all subclasses that inherit from the target type
+        /// Retrieve attributes of target type in all loaded assemblies
         /// </summary>
-        /// <param name="baseType"></param>
+        /// <param name="attributeType"></param>
         /// <returns></returns>
-        public static Type[] GetAllSubtypes(Type baseType)
+        public static DirectRetrieveAttribute[] GetAllAttributes(Type attributeType)
         {
-            //foreach(var t in lut.Value.ToArray().SelectMany(a => a.Value)) {
-            //    Console.WriteLine($"t: {t.targetType}");
-            //}
-            return typeSet.Value
-                .Where(a=>a!=baseType && baseType.IsAssignableFrom(a))
+#if DEBUG
+            CheckAttribute(attributeType);
+#endif
+            return attrLut.Value.ToArray()
+                .Where(a=> attributeType.IsAssignableFrom(a.Key))
+                .SelectMany(a=>a.Value)
                 .ToArray()
                 ;
         }
         /// <summary>
-        /// Retrieve Attribute T on a specific Assembly
+        /// Retrieve attributes of target type in a specific assembly
+        /// </summary>
+        /// <param name="attributeType"></param>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static DirectRetrieveAttribute[] GetAllAttributes(Type attributeType, Assembly assembly)
+        {
+#if DEBUG
+            CheckAttribute(attributeType);
+#endif
+            return assembly.GetCustomAttributes<GeneratedDirectRetrieveAttribute>()
+                .SelectMany(a => {
+                    var targetType = a.type;
+                    if (a.HasMemberName)
+                    {
+                        return targetType.GetMember(a.memberName, bindingFlags).SelectMany(
+                            m => m.GetCustomAttributes(attributeType,false).Select(
+                                ca => SetAttributeValue(ca as DirectRetrieveAttribute, targetType, m)
+                            )
+                        );
+                    }
+                    else
+                    {
+                        return targetType.GetCustomAttributes(attributeType,false).Select(
+                            ca => SetAttributeValue(ca as DirectRetrieveAttribute, targetType, null)
+                        );
+                    }
+                })
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Retrieve attributes of type T in a specific assembly
         /// </summary>
         /// <param name="assembly"></param>
         /// <typeparam name="T"></typeparam>
@@ -104,29 +133,54 @@ namespace com.bbbirder
                 })
                 .ToArray();
         }
+
+
         /// <summary>
         /// Retrieve all subclasses that inherit from the target type
+        /// </summary>
+        /// <param name="baseType"></param>
+        /// <returns></returns>
+        public static Type[] GetAllSubtypes(Type baseType)
+        {
+#if DEBUG
+            CheckBasetype(baseType);
+#endif
+            return typeSet.Value
+                .Where(a=>a!=baseType && baseType.IsAssignableFrom(a))
+                .ToArray()
+                ;
+        }
+
+        /// <summary>
+        /// Retrieve all subclasses that inherit from the target type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Type[] GetAllSubtypes<T>()
+        {
+            return GetAllSubtypes(typeof(T));
+        }
+
+        /// <summary>
+        /// Retrieve all subclasses that inherit from the target type in a specific assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Type[] GetAllSubtypes<T>(Assembly assembly){
+            return GetAllSubtypes(typeof(T),assembly);
+        }
+
+        /// <summary>
+        /// Retrieve all subclasses that inherit from the target type in a specific assembly
         /// </summary>
         /// <param name="assembly"></param>
         /// <param name="baseType"></param>
         /// <returns></returns>
-        public static Type[] GetAllSubtypes(Assembly assembly, Type baseType)
+        public static Type[] GetAllSubtypes(Type baseType, Assembly assembly)
         {
 #if DEBUG
-            var attributesOnBase = baseType.GetCustomAttributes(true)
-                .OfType<DirectRetrieveAttribute>()
-                .ToArray();
-            if (attributesOnBase.Length == 0)
-            {
-                throw new($"there is no DirectRetrieveAttribute on type {baseType}");
-            }
-            var hasInherit = attributesOnBase
-                .Select(a => a.GetType().GetCustomAttribute<AttributeUsageAttribute>())
-                .Any(usage => usage.Inherited);
-            if (!hasInherit)
-            {
-                throw new($"DirectRetrieveAttribute on {baseType} should be Inherited");
-            }
+            CheckBasetype(baseType);
 #endif
             return assembly.GetCustomAttributes<GeneratedDirectRetrieveAttribute>()
                 .Where(a => IsBaseType(a.type,baseType))
@@ -143,6 +197,30 @@ namespace com.bbbirder
                 }
             }
 
+        }
+
+
+        static void CheckAttribute(Type attributeType){
+            if (!typeof(DirectRetrieveAttribute).IsAssignableFrom(attributeType))
+            {
+                throw new($"type {attributeType} is not a DirectRetrieveAttribute");
+            }
+        }
+        static void CheckBasetype(Type baseType){
+            var attributesOnBase = baseType.GetCustomAttributes(true)
+                .OfType<DirectRetrieveAttribute>()
+                .ToArray();
+            if (attributesOnBase.Length == 0)
+            {
+                throw new($"there is no DirectRetrieveAttribute on type {baseType}");
+            }
+            var hasInherit = attributesOnBase
+                .Select(a => a.GetType().GetCustomAttribute<AttributeUsageAttribute>())
+                .Any(usage => usage.Inherited);
+            if (!hasInherit)
+            {
+                throw new($"DirectRetrieveAttribute on {baseType} should be Inherited");
+            }
         }
         ///// <summary>
         ///// Retrieve all classes that implement the target interface
