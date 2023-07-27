b59361cf17afe010914406b33c1f364379c2ab41
labbbirder <502100554@qq.com>
2023-07-27 21:05:24 +0800

    fix: Attribute preseved by default

--- Runtime/InheritRetrieveAttribute.cs
+++ Runtime/InheritRetrieveAttribute.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine.Scripting;
 
 namespace com.bbbirder {
+    [Preserve]
     [AttributeUsage(AttributeTargets.Class|AttributeTargets.Interface,Inherited =true)]
     public sealed partial class InheritRetrieveAttribute : DirectRetrieveAttribute{
         
